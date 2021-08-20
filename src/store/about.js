8b59c93f647bb05a94c44b253cc55b697cac14e2
Namgyung2 <79958830+Namgyung2@users.noreply.github.com>
2021-08-20 11:26:54 +0900

    Modify server port number

--- src/store/about.js
+++ src/store/about.js
@@ -1,10 +1,10 @@
 export default {
   namespaced: true,
   state: () => ({
-    name: 'HEROPY',
-    email: 'thesecon@gmail.com',
-    blog: "https://heropy.blog",
-    phone: "+82-10-1234-5678",
+    name: 'Namgyung Kim',
+    email: 'namgyung.kim@gmail.com',
+    blog: "Non",
+    phone: "+82-10-0000-0000",
     image: 'https://heropy.blog/css/images/logo.png'
   })
 }
